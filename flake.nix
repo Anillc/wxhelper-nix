564e7d2e0b1f3aef82e53d59842e343d089c6d6f
Anillc <i@anillc.cn>
2024-08-30 01:35:17 +0800

    add flakeModules

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   outputs = inputs@{
     self, nixpkgs, flake-parts,
-  }: flake-parts.lib.mkFlake { inherit inputs; } {
+  }: let
+    modules = map (x: ./modules/${x}) (nixpkgs.lib.attrNames (builtins.readDir ./modules));
+  in flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     perSystem = { config, pkgs, lib, ... }: {
-      imports = map (x: ./modules/${x}) (lib.attrNames (builtins.readDir ./modules));
+      imports = modules;
       packages.default = config.wxhelper.wxhelper;
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [];
       };
+    };
+    flake.flakeModules.default = {
+      imports = modules;
     };
   };
 }
